eb730bd4e57647eb5e38e0044b058ba3dbc2fa59
Anna Moura Sobral <annamourasobral@gmail.com>
2022-03-30 15:44:10 +0200

    ignoring node

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import "./intro.css"
 const Intro = () => {
     return (
         <div className="i">
-            <div className="i-left">left</div>
+            <div className="i-left">left
+            <div className="i-left-wrapper">
+                </div>
+                <h2 className="i-intro">Hello, my name is</h2>
+                <h1 className="i-name">Anna Moura Sobral</h1>
+                </div>
             <div className="i-rigth">right</div>
         </div>
     )
