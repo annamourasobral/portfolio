bd43350649aae2952059e0b58d5c8468caaa2791
Anna Moura Sobral <annamourasobral@gmail.com>
2022-04-01 20:48:34 +0200

    centralizing text in about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,17 +10,19 @@ const About = () => {
                 </div>
             </div>
             <div className="about-right">
-                <h1 className="about-title">About Me</h1>
-                <p className="about-sub">
-                    It is a long established fact that a reader will be distracted by the
-                    readable content.
-                </p>
-                <p className="about-desc">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
-                </p>
+                <div className="about-right-wrapper">
+                    <h1 className="about-title">About Me</h1>
+                    <p className="about-sub">
+                        It is a long established fact that a reader will be distracted by the
+                        readable content.
+                    </p>
+                    <p className="about-desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+                    </p>
+                </div>
             </div>
         </div>
     );
