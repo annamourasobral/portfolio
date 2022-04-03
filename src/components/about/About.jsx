96d0d264e872b3e1e82595e159bdb0c392ec8867
Anna Moura Sobral <annamourasobral@gmail.com>
2022-04-03 19:49:56 +0200

    adding dark mode toggle

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,27 +1,33 @@
 import "./about.css";
 
 const About = () => {
-    return (
-        <div className="about">
-            <div className="about-left">
-                <div className="about-card-bg"></div>
-                <div className="about-card">
-                    <img src="https://images.pexels.com/photos/1804177/pexels-photo-1804177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="about-img"/>
-                </div>
-            </div>
-            <div className="about-right">
-                <div className="about-right-wrapper">
-                    <h1 className="about-title">About Me</h1>
-                    <p className="about-sub">
-                        I'm from Rio de Janeiro, Brazil, and love cultural exchanges, travels and of course, its records in form of photograph.
-                    </p>
-                    <p className="about-desc">
-                        Living and following the beauty in every corner of the world I grow a repertory to bring more life and joy to my work.
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://r1.ilikewallpaper.net/iphone-11-pro-max-wallpapers/download/96085/Rio-de-Janeiro-sunrise-sky-5k-Travel-iphone-11-pro-max-wallpaper-ilikewallpaper_com_1242x2688.jpg"
+            alt=""
+            className="a-img"
+          />
         </div>
-    );
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
