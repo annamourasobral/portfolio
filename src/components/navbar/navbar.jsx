b360f7ac66bb1f83267970d882fe8abced21b2c6
Anna Moura Sobral <annamourasobral@gmail.com>
2022-05-04 21:39:58 +0200

    save to continue

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,25 @@ import "./navbar.css";
 
 const navbar = () => {
     return (
-        <nav>
-        </nav>
+        <Nav>
+            <NavLink to="/">
+                <h1>Logo</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/About" activeStyle>
+                    About Me
+                </NavLink>
+                <NavLink to="/productList" activeStyle>
+                    Portfolio
+                </NavLink>
+                <NavLink to="/Contact" activeStyle>
+                    Contact
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink></NavBtnLink>
+            </NavBtn>
+        </Nav>
     );
 }
