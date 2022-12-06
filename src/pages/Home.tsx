18b82498519890f6b2a0af54baabcbafe8ca7c91
Anna Moura Sobral <annamourasobral@gmail.com>
2022-12-06 16:55:22 +0100

    home text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../components/Navbar';
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: var(--urban);
+`;
+
+const Hello = styled.p`
+    font-size: 5rem;
+`;
+
+const Title = styled.p`
+    font-size: 2rem;
+`;
+
+const Motivation = styled.p`
+    font-size: 3 rem;
+`;
 
 const Home = () => {
     return (
         <div id='home' className='page-container'>
             <Navbar />
 
-            <div>
-                <p>hello</p>
-                <p>I'm a frontend developer</p>
-                <p>where the logic thinking is beautiful</p>
-            </div>
+            <TextWrapper>
+                <Hello>hello</Hello>
+                <Title>I'm a frontend developer</Title>
+                <Motivation>
+                    passionate and motivated by the impact of my code
+                </Motivation>
+            </TextWrapper>
         </div>
     );
 };
