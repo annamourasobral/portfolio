c0ad8ed1082c449ba03927a4acd546226c87d506
Anna Moura Sobral <annamourasobral@gmail.com>
2022-12-07 23:45:00 +0100

    changed font

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import Navbar from '../components/Navbar';
-
-const TextWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: var(--urban);
-`;
-
-const Hello = styled.p`
-    font-size: 5rem;
-`;
-
-const Title = styled.p`
-    font-size: 2rem;
-`;
-
-const Motivation = styled.p`
-    font-size: 3 rem;
-`;
 
 const Home = () => {
     return (
         <div id='home' className='page-container'>
             <Navbar />
 
-            <TextWrapper>
-                <Hello>hello</Hello>
-                <Title>I'm a frontend developer</Title>
-                <Motivation>
-                    passionate and motivated by the impact of my code
-                </Motivation>
-            </TextWrapper>
+            <div id='text-wrapper'>
+                <span>I'm Anna Moura Sobral a passionate</span>
+                <span id='fz'>frontend developer</span>
+            </div>
         </div>
     );
 };
