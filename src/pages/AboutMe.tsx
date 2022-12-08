cad4ae9eb2d634c1b69e0ffd6431248553c74035
Anna Moura Sobral <annamourasobral@gmail.com>
2022-12-08 23:45:00 +0100

    home changes

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import avatar from '../assets/avatar.jpg';
 
 const AboutMe = () => {
-    return (
-        <div id='about-me' className='page-container'>
-            <img id='avatar' src={avatar} alt='avatar' />
-        </div>
-    );
+    return <div id='about-me' className='page-container'></div>;
 };
 
 export default AboutMe;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import avatar from '../assets/avatar.jpg';
 
 const Home = () => {
     return (
         <div id='home' className='page-container'>
             <Navbar />
-
-            <div id='text-wrapper'>
-                <span>I'm Anna Moura Sobral a passionate</span>
-                <span id='fz'>frontend developer</span>
+            <div id='wrapper'>
+                <div id='text-wrapper'>
+                    <span id='line'></span>
+                    <span>I'm Anna Moura Sobral a passionate</span>
+                    <span id='fz'>frontend developer</span>
+                </div>
+                <img id='avatar' src={avatar} alt='avatar' />
             </div>
         </div>
     );
