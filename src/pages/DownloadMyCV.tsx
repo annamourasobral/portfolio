71632758095f55f69dcaa567a93e7888877134af
Anna Moura Sobral <annamourasobral@gmail.com>
2022-12-11 22:40:00 +0100

    adding my CV

--- src/pages/DownloadMyCV.tsx
+++ src/pages/DownloadMyCV.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const DownloadMyCV = () => {
-    return (
-        <div id='my-cv' className='page-container'>
-            DownloadMyCV
-        </div>
-    );
+    return <div id='my-cv' className='page-container'></div>;
 };
 
 export default DownloadMyCV;
