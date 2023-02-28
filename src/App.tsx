45d877450f751916bf41c2d01988c2edc24dcb42
Anna Moura Sobral <annamourasobral@gmail.com>
2023-02-28 21:52:03 +0100

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import AboutMe from './pages/AboutMe';
-import ContactMe from './pages/ContactMe';
-import Skills from './pages/Skills';
-// import Projects from './pages/Projects';
-import DownloadMyCV from './pages/DownloadMyCV';
-import Home from './pages/Home';
 
 function App() {
-    return (
-        <div>
-            <Home />
-            <AboutMe />
-            {/* <Projects /> */}
-            <Skills />
-            <ContactMe />
-            <DownloadMyCV />
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
--- src/pages/DownloadMyCV.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DownloadMyCV = () => {
-    return <div id='my-cv' className='page-container'></div>;
-};
-
-export default DownloadMyCV;
